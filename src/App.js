6f4a3bc5644c495090f3cc433676236dd1f7fb03
Sai Bingi <saibingi1208@gmail.com>
2022-05-18 18:49:40 +0000

    Cryptocurrency Tracker Solution

---
+++ src/App.js
@@ -0,0 +1,9 @@
+// Published URL ==> https://cryptocurrencie.ccbp.tech
+
+import CryptocurrencyTracker from './components/CryptocurrencyTracker'
+
+import './App.css'
+
+const App = () => <CryptocurrencyTracker />
+
+export default App
